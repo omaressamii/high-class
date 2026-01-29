34ed75de3ea9dac6678ac9d1f9bab8da3453e8b7
omaressamii <omaressamit@gmail.com>
2026-01-29 17:15:42 +0200

    main

--- src/lib/mock-data.ts
+++ src/lib/mock-data.ts
@@ -61,7 +61,7 @@ export const productTypeValues: ProductTypeDefinition[] = [
   { id: 'dress', name: 'Dress', name_ar: 'فستان' },
 ];
 export const productCategoryValues: ProductCategory[] = ['Rental', 'Sale'];
-export const productSizeValues: ProductSize[] = ['36', '37', '38', '39', '40', '41', '42', '43', '44', '45', '46', '47', '48', '49', '50', '51', '52', '53', '54', '55', '56', '57', '58', '59', '60', '61', '62', '63', '64', '65', '66', '67', '68', 'Custom'];
+export const productSizeValues: ProductSize[] = ['18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40', '41', '42', '43', '44', '45', '46', '47', '48', '49', '50', '51', '52', '53', '54', '55', '56', '57', '58', '59', '60', '61', '62', '63', '64', '65', '66', '67', '68', 'Custom'];
 export const productStatusValues: ProductStatus[] = ['Available', 'Rented', 'Sold'];
 
 
