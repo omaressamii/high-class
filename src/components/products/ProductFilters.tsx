d45c1a2063a34eb3a578619c120e953422fedc94
omaressamii <omaressamit@gmail.com>
2026-01-29 16:17:00 +0200

    Main

--- src/components/products/ProductFilters.tsx
+++ src/components/products/ProductFilters.tsx
@@ -24,7 +24,7 @@ interface ProductFiltersProps {
 // const productTypeValues: ProductType[] = ['Suit', 'Dress']; // Removed
 const productCategoryValues: ProductCategory[] = ['Rental', 'Sale'];
 const productStatusValues: ProductStatus[] = ['Available', 'Rented', 'Sold'];
-const productSizeValues: ProductSize[] = ['36', '37', '38', '39', '40', '41', '42', '43', '44', '45', '46', '47', '48', '49', '50', '51', '52', '53', '54', '55', '56', '57', '58', '59', '60', '61', '62', '63', '64', '65', '66', '67', '68', 'Custom'];
+const productSizeValues: ProductSize[] = ['18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40', '41', '42', '43', '44', '45', '46', '47', '48', '49', '50', '51', '52', '53', '54', '55', '56', '57', '58', '59', '60', '61', '62', '63', '64', '65', '66', '67', '68', 'Custom'];
 
 
 export function ProductFilters({ filters, setFilters, lang: propLang, availableProductTypes }: ProductFiltersProps) {
